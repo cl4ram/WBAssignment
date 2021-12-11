a835c88c18fe9a4dd72620b1a1072c31fa3c8428
cl4ram <molinayvediaclara@outlook.com>
2021-12-11 11:20:39 -0300

    responsive transitions

--- script.js
+++ script.js
@@ -7,9 +7,13 @@ closeMenu.addEventListener('click',close);
 
 function show(){
     mainMenu.style.display = 'flex';
+    mainMenu.style.visibility = 'visible'
+    mainMenu.style.opacity = '1'
     mainMenu.style.top = '0';
 }
 
 function close(){
-    mainMenu.style.display = 'none'
+    mainMenu.style.opacity = '0'
+    mainMenu.style.visibility = 'hidden'
+    mainMenu.style.transition = 'all 0.3s linear'
 }
